ea20e79c4792690df6fed40a8360e9d22104870c
smalameen <smalameen0@gmail.com>
2020-08-15 20:18:32 +0600

    three work Done

--- weatherApp.js
+++ weatherApp.js
@@ -1,33 +1,53 @@
-const api = {
-    key :"4a640ca36a6563d09e7a09a8517134ac",
-    base : "https://api.openweathermap.org/data/2.5/"
-}
-
 function gettingDateByClick(){
     const getData = document.getElementById("search-button");
     const getDataOne = getData.value;
     console.log(getDataOne);
     getDataFromFetch();
+    displayOutput();
     
 }
-// function getInputFromUser(){
-   
-//     getDataFromFetch();
-// }
+
+
+
+
 function getDataFromFetch(){
     const areInput = document.getElementById("input-from-user");
     const inputValue = areInput.value;
-fetch (`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=celsius&APPID=4a640ca36a6563d09e7a09a8517134ac`)
-    .then(res => res.json())
-    .then(weather => console.log(weather))
+fetch (`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&APPID=4a640ca36a6563d09e7a09a8517134ac`)
+    .then(response => response.json())
+    
+    .then (weather => displayData(weather))
+    
 
-    .then(showInput());
+    function displayData(weather){
+        
+        const nameValue = document.getElementById("myCity");
+        nameValue.innerText = `${weather.name}`;
+
+        const tempValue = document.getElementById("temperature");
+        tempValue.innerHTML =`${Math.round(weather.main.temp)}<span>&deg;C</span>`;
+
+        const getDescription = document.getElementById("description");
+        getDescription.innerHTML = weather.weather[0].main;
+
+         
+        
+        deleteImage();
+
+    }
+    
+         
+        
 }
 
-function showInput(name){
-    let myCity = document.getElementById("myCity");
-    myCity.innerText = `${name}`;
+function deleteImage(){
+    document.getElementById("image-change").style.display = "none";
 }
+
+
+    
+
+
         
 
 
